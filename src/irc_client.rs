680a659149ff0ff750b9a9e75b4e6fe599c82d66
Eunchong Yu <kroisse@gmail.com>
2017-08-01 22:46:43 +0900

    Entering into the shitty future
    Follow https://twitter.com/shitty_future

--- src/irc_client.rs
+++ src/irc_client.rs
@@ -1,10 +1,11 @@
 use std::thread;
 
+use futures::{Future, Stream, lazy};
 use irc::client::prelude::*;
 use slog;
 
-use ::Result;
-use message::{Bus, Message, MessageCreated, Payload};
+use ::{Error, Result};
+use message::{Bus, BusSender, Message, MessageCreated, Payload};
 
 
 pub struct Irc {
@@ -12,46 +13,59 @@ pub struct Irc {
 }
 
 impl Irc {
-    pub fn from_config<L>(logger: L, bus: Bus, cfg: Config) -> Result<Irc>
+    pub fn from_config<L>(logger: L, bus: Bus, cfg: Config) -> impl Future<Item=Irc, Error=Error>
         where L: Into<Option<slog::Logger>>
     {
         let logger = logger.into().unwrap_or_else(|| slog::Logger::root(slog::Discard, o!()));
-        let client = IrcServer::from_config(cfg)?;
-        client.identify()?;
         let log = logger.new(o!());
-        let c = client.clone();
         let sender = bus.sender();
-        thread::spawn(move || {
-            c.for_each_incoming(|message| {
-                debug!(log, "{}", message);
-                let nickname = message.source_nickname()
-                    .map(String::from)
-                    .unwrap_or_else(String::new);
-                match message.command {
-                    Command::PRIVMSG(target, content) => {
-                        let mut m = Message::MessageCreated(MessageCreated {
-                            nickname,
-                            channel: target,
-                            content,
-                        });
-                        while let Err(e) = sender.try_send(m) {
-                            use std::sync::mpsc::TrySendError::*;
-                            m = match e {
-                                Full(p) => p,
-                                Disconnected(_) => { panic!("bus closed"); }
-                            };
-                            thread::yield_now();
-                        }
-                    }
-                    _ => { }
-                }
-            }).unwrap();
-        });
-        spawn_actor(logger, client.clone(), bus);
-        Ok(Irc {
-            client,
+        lazy::<_, Result<_>>(move || {
+            let client = IrcServer::from_config(cfg)?;
+            client.identify()?;
+            spawn_actor(logger, client.clone(), bus);
+            Ok(client)
+        }).and_then(move |client| {
+            let c = client.clone();
+            listen_messages(c, log, sender)
+                .map(move |_| client)
+        }).map(move |client| {
+            Irc {
+                client,
+            }
         })
     }
+}
+
+fn listen_messages(
+    client: IrcServer,
+    log: slog::Logger,
+    sender: BusSender,
+) -> impl Future<Item=(), Error=Error> {
+    client.stream().for_each(move |message| {
+        debug!(log, "{}", message);
+        let nickname = message.source_nickname()
+            .map(String::from)
+            .unwrap_or_else(String::new);
+        match message.command {
+            Command::PRIVMSG(target, content) => {
+                let mut m = Message::MessageCreated(MessageCreated {
+                    nickname,
+                    channel: target,
+                    content,
+                });
+                while let Err(e) = sender.try_send(m) {
+                    use std::sync::mpsc::TrySendError::*;
+                    m = match e {
+                        Full(p) => p,
+                        Disconnected(_) => { panic!("bus closed"); }
+                    };
+                    thread::yield_now();
+                }
+            }
+            _ => { }
+        }
+        Ok(())
+    }).map_err(From::from)
 }
 
 fn spawn_actor(logger: slog::Logger, client: IrcServer, bus: Bus) {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#![feature(conservative_impl_trait)]
+
+#[macro_use] extern crate error_chain;
 #[macro_use] extern crate slog;
 
 extern crate discord;
+extern crate futures;
 extern crate irc;
 extern crate multiqueue;
 extern crate slog_async;
@@ -18,11 +22,19 @@ use std::process;
 use std::thread;
 use std::time::Duration;
 
+use futures::Future;
 use irc::client::data::Config;
 use slog::*;
 
-type Error = Box<::std::error::Error>;
-type Result<T> = ::std::result::Result<T, Error>;
+
+error_chain! {
+    links {
+        Irc(irc::error::Error, irc::error::ErrorKind);
+    }
+    foreign_links {
+        Discord(discord::Error);
+    }
+}
 
 
 fn main() {
@@ -42,7 +54,7 @@ fn run() -> Result<()> {
 
     let bus = message::Bus::root();
 
-    let discord_bot_token = env::var("DISCORD_BOT_TOKEN")?;
+    let discord_bot_token = env::var("DISCORD_BOT_TOKEN").chain_err(|| "")?;
 
     let discord_bus = bus.add();
     let discord_bus_id = discord_bus.id;
@@ -53,28 +65,29 @@ fn run() -> Result<()> {
     let cfg = Config {
         nickname: Some(format!("Rendezvous")),
         server: Some(format!("irc.ozinger.org")),
-        use_ssl: Some(true),
-        port: Some(6697),
+        use_ssl: Some(false),
+        port: Some(6667),
         channels: Some(channels.into_iter().map(|ch| format!("#{}", ch.name)).collect()),
         umodes: Some("+Bx".to_owned()),
         .. Default::default()
     };
     let irc_bus = bus.add();
     let irc_bus_id = irc_bus.id;
-    irc_client::Irc::from_config(log.new(o!()), irc_bus, cfg)?;
-
-    for payload in bus {
-        use message::Message::*;
-        match payload.message {
-            MessageCreated(msg) => {
-                if payload.sender == discord_bus_id {
-                    info!(log, "from Discord {} {}: {}", msg.channel, msg.nickname, msg.content);
-                } else if payload.sender == irc_bus_id {
-                    info!(log, "from IRC {} {}: {}", msg.channel, msg.nickname, msg.content);
+    irc_client::Irc::from_config(log.new(o!()), irc_bus, cfg)
+        .and_then(|_| {
+            for payload in bus {
+                use message::Message::*;
+                match payload.message {
+                    MessageCreated(msg) => {
+                        if payload.sender == discord_bus_id {
+                            info!(log, "from Discord {} {}: {}", msg.channel, msg.nickname, msg.content);
+                        } else if payload.sender == irc_bus_id {
+                            info!(log, "from IRC {} {}: {}", msg.channel, msg.nickname, msg.content);
+                        }
+                    }
                 }
             }
-        }
-    }
-
-    Ok(())
+            Ok(())
+        })
+        .wait()
 }

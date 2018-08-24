7adf91ae148f3c9f5f5cf08639bb32a0d9e58321
Eunchong Yu <kroisse@gmail.com>
2018-08-24 19:29:50 +0900

    Add config for nickname rewrite

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::sync::{Arc};
@@ -6,6 +7,7 @@ use std::path::Path;
 use irc::client::data::Config as IrcConfig;
 use lazy_static::lazy_static;
 use parking_lot::RwLock;
+use regex::Regex;
 use serde_derive::Deserialize;
 
 use crate::Error;
@@ -19,6 +21,10 @@ pub struct Config {
     pub discord: DiscordConfig,
     #[serde(default)]
     pub irc: IrcConfig,
+
+    #[serde(default)]
+    #[serde(with = "bots")]
+    pub bots: HashMap<String, Regex>,
 }
 
 #[derive(Clone, Default, Deserialize)]
@@ -49,4 +55,22 @@ pub fn update(mut config: Config) {
 
 pub fn fetch_config() -> Arc<Config> {
     Arc::clone(&CONFIG.read())
+}
+
+mod bots {
+    use serde::de;
+
+    use super::*;
+
+    #[inline]
+    pub(super) fn deserialize<'de, D>(de: D) -> Result<HashMap<String, Regex>, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let s: HashMap<String, &str> = de::Deserialize::deserialize(de)?;
+        let m = s.into_iter()
+            .map(|(k, v)| Regex::new(v).map(|v| (k, v)))
+            .collect::<Result<_, _>>();
+        m.map_err(de::Error::custom)
+    }
 }

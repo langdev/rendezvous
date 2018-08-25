533682f52016e693b5e3d70777a0ae10ecc1c381
Eunchong Yu <kroisse@gmail.com>
2018-08-26 00:55:45 +0900

    Add tests for SubscriptionList

--- src/macros.rs
+++ src/macros.rs
@@ -9,3 +9,48 @@ macro_rules! impl_get_bus_id {
         }
     }
 }
+
+#[macro_export]
+macro_rules! sleep_millis {
+    ($millis:expr) => ({
+        let d = ::std::time::Duration::from_millis($millis);
+        let f = ::tokio::timer::Delay::new(Instant::now() + d).compat();
+        await!(f)
+    })
+}
+
+
+#[cfg(test)]
+#[macro_use]
+mod test {
+    #[macro_export]
+    macro_rules! actix_test_cases {
+        { $(async fn $name:ident() $body:block)+ } => {
+            $(
+                #[test]
+                fn $name() {
+                    async fn test() {
+                        $body
+                    }
+
+                    assert_eq!(::actix::System::run(||{
+                        use futures::{compat::*, prelude::*};
+                        use tokio::prelude::{
+                            Future as Future01,
+                            FutureExt as Future01Ext,
+                        };
+
+                        let f = test().unit_error().boxed().compat(TokioDefaultSpawn);
+                        let f = f.deadline(Instant::now() + Duration::from_secs(5));
+
+                        Arbiter::spawn(f.then(|r| {
+                            r.unwrap();
+                            System::current().stop();
+                            Ok(())
+                        }));
+                    }), 0);
+                }
+            )*
+        }
+    }
+}

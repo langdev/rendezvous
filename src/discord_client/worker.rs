172efb3c313fc1466488a9cd691862e40b000055
Hyeon Kim <simnalamburt@gmail.com>
2018-12-11 22:05:38 +0900

    Choose between Webhook and bot API depending on 'SendMessage'

--- src/discord_client/worker.rs
+++ src/discord_client/worker.rs
@@ -2,9 +2,8 @@ use std::thread;
 use std::time::Duration;
 
 use ::actix::prelude::*;
-use ::serenity::{
-    model::prelude::*,
-};
+use serenity::model::prelude::*;
+use serenity::http;
 
 use crate::Error;
 
@@ -39,19 +38,38 @@ impl Handler<SendMessage> for DiscordWorker {
     type Result = Result<(), Error>;
 
     fn handle(&mut self, msg: SendMessage, _: &mut Self::Context) -> Self::Result {
-        let SendMessage { channel, content, .. } = msg;
-        while let Err(e) = channel.send_message(|msg| msg.content(content.clone())) {
-            use serenity::Error::*;
-            match e {
-                Http(..) | Hyper(..) | WebSocket(..) => {
-                    thread::sleep(Duration::from_millis(100));
+        use serenity::Error::{Http, Hyper, WebSocket};
+
+        // TODO: 설정을 어딘가 괜찮은곳에 저장해야함
+        let id = 245037420704169985;
+        let token = "ig5AO-wdVWpCBtUUMxmgsWryqgsW3DChbKYOINftJ4DCrUbnkedoYZD0VOH1QLr-S3sV";
+        let webhook = http::get_webhook_with_token(id, token).expect("valid webhook");
+
+        loop {
+            // 메세지 전송
+            let result = if let Some(nick) = &msg.nickname {
+                // 닉네임이 있음; Webhook을 사용해 전송함
+                webhook.execute(true, |w| w.content(&msg.content).username(nick)).map(|_| ())
+            } else {
+                // 닉네임이 없음; Bot API를 사용해 전송함
+                msg.channel.send_message(|m| m.content(&msg.content)).map(|_| ())
+            };
+
+            match result {
+                // 성공할경우 결과 반환
+                Ok(()) => {
+                    return Ok(())
                 }
-                _ => {
-                    return Err(From::from(e));
+                // 다음 에러중 하나일경우 일정시간 대기 후 재시도
+                Err(Http(_)) | Err(Hyper(_)) | Err(WebSocket(_)) => {
+                    thread::sleep(Duration::from_millis(100))
+                }
+                // 모르는 유형의 에러일경우 즉시 포기
+                Err(e) => {
+                    return Err(From::from(e))
                 }
             }
         }
-        Ok(())
     }
 }
 

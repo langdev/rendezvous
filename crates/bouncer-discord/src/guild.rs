bc8f7ac8a4befc6d904ba453281a5f41fe41bf45
Eunchong Yu <kroisse@gmail.com>
2019-09-18 14:08:38 +0900

    Test to test that tarpaulin works

--- crates/bouncer-discord/src/guild.rs
+++ crates/bouncer-discord/src/guild.rs
@@ -44,12 +44,43 @@ impl From<GuildMemberUpdateEvent> for UserData {
 
 fn author_nickname<'a>(g: &'a GuildMap, message: &Message) -> Option<&'a str> {
     let guild = g.get(&message.guild_id?)?;
-    Some(guild
-        .members
-        .get(&message.author.id)?
-        .name.as_str())
+    Some(guild.members.get(&message.author.id)?.name.as_str())
 }
 
 pub fn author_name<'a>(g: &'a GuildMap, message: &'a Message) -> &'a str {
     author_nickname(g, message).unwrap_or_else(|| &message.author.name[..])
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn author_nickname_empty_guild_map() {
+        let guild_map = GuildMap::new();
+        let message = serde_json::from_value::<Message>(json!({
+            "id": 42,
+            "attachments": [],
+            "author": {
+                "id": 197,
+                "discriminator": "1234",
+                "username": "James"
+            },
+            "channel_id": 65234,
+            "content": "Hello, world",
+            "embeds": [],
+            "guild_id": 100200,
+            "type": 0,
+            "mention_everyone": false,
+            "mention_roles": [],
+            "mentions": [],
+            "pinned": false,
+            "timestamp": "2019-09-18T12:34:56Z",
+            "tts": false
+        }))
+        .unwrap();
+        assert!(author_nickname(&guild_map, &message).is_none());
+    }
 }

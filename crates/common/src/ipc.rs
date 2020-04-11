105d213dc5200248019f404cdc06552eb316127d
Eunchong Yu <kroisse@gmail.com>
2020-04-12 05:54:08 +0900

    Add test about spawn_socket

--- crates/common/src/ipc.rs
+++ crates/common/src/ipc.rs
@@ -6,6 +6,7 @@ use nng::{
     options::{protocol::pair::Polyamorous, Options},
     Error, Message, PipeEvent, Protocol, Socket,
 };
+use serde::{de::DeserializeOwned, ser::Serialize};
 use tracing::info;
 
 use crate::data::Event;
@@ -15,6 +16,15 @@ pub fn address() -> String {
 }
 
 pub fn spawn_socket(
+    msg_tx: mpsc::Sender<Event>,
+    event_rx: mpsc::Receiver<Event>,
+) -> Result<(), Error> {
+    let address = address();
+    spawn_socket_impl(&address, msg_tx, event_rx)
+}
+
+fn spawn_socket_impl(
+    address: &str,
     msg_tx: mpsc::Sender<Event>,
     event_rx: mpsc::Receiver<Event>,
 ) -> Result<(), Error> {
@@ -28,10 +38,18 @@ pub fn spawn_socket(
         _ => {}
     })?;
 
-    let address = address();
+    dial(&socket, address)?;
 
+    let s = socket.clone();
+    thread::spawn(move || receive_from_socket(s, msg_tx));
+    thread::spawn(move || send_to_socket(socket, event_rx));
+
+    Ok(())
+}
+
+fn dial(socket: &Socket, address: &str) -> Result<(), Error> {
     loop {
-        match socket.dial(&address) {
+        match socket.dial(address) {
             Ok(_) => {
                 break;
             }
@@ -44,27 +62,125 @@ pub fn spawn_socket(
         }
         info!("Reconnecting...");
     }
+    Ok(())
+}
 
-    let mut msg = Message::new();
-
-    let s = socket.clone();
-    thread::spawn(move || loop {
-        let msg = s.recv().unwrap();
+fn receive_from_socket<T: DeserializeOwned>(socket: Socket, tx: mpsc::Sender<T>) {
+    loop {
+        let msg = socket.recv().unwrap();
         info!("Recv");
         if let Ok(event) = serde_cbor::from_slice(msg.as_slice()) {
-            msg_tx.send(event).unwrap();
-        }
-    });
-
-    thread::spawn(move || {
-        for e in event_rx {
-            msg.clear();
-            if serde_cbor::to_writer(&mut msg, &e).is_ok() {
-                info!("sending...");
-                socket.send(msg.clone()).map_err(nng::Error::from).unwrap();
+            if let Err(_) = tx.send(event) {
+                info!("channel closed");
+                break;
             }
         }
-    });
+    }
+}
 
-    Ok(())
+fn send_to_socket<T: Serialize>(socket: Socket, rx: mpsc::Receiver<T>) {
+    let mut msg = Message::new();
+    for e in rx {
+        msg.clear();
+        if serde_cbor::to_writer(&mut msg, &e).is_ok() {
+            info!("sending...");
+            socket.send(msg.clone()).map_err(nng::Error::from).unwrap();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use parking_lot::Mutex;
+
+    use super::*;
+
+    fn socket_pair1_new() -> Result<Socket, Error> {
+        let sock = Socket::new(Protocol::Pair1)?;
+        sock.set_opt::<Polyamorous>(true)?;
+        Ok(sock)
+    }
+
+    enum Command {
+        Notify {
+            pipe: nng::Pipe,
+            event: nng::PipeEvent,
+        },
+        Close,
+    }
+
+    #[test]
+    fn receive_basic() {
+        let (msg_tx, msg_rx) = mpsc::channel();
+
+        let (tx, rx) = mpsc::channel();
+        let addr = "inproc://receive_basic";
+        let pipe_tx = Mutex::new(Some(tx.clone()));
+        thread::spawn(move || {
+            let srv_sock = socket_pair1_new().unwrap();
+            srv_sock
+                .pipe_notify(move |pipe, event| {
+                    let mut tx = pipe_tx.lock();
+                    if let Some(t) = tx.as_ref() {
+                        if let Err(_) = t.send(Command::Notify { pipe, event }) {
+                            *tx = None;
+                        }
+                    }
+                })
+                .unwrap();
+            srv_sock.listen(addr).unwrap();
+            for cmd in rx {
+                match cmd {
+                    Command::Notify {
+                        pipe,
+                        event: PipeEvent::AddPost,
+                    } => {
+                        let mut m = Message::new();
+                        m.set_pipe(pipe);
+                        serde_cbor::to_writer(&mut m, &"Hello client").unwrap();
+                        srv_sock.send(m).unwrap();
+                    }
+                    Command::Close => {
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+            srv_sock.close();
+        });
+        let _guard = scopeguard::guard((), move |_| {
+            let _ = tx.send(Command::Close);
+        });
+
+        let client_sock = socket_pair1_new().unwrap();
+        dial(&client_sock, addr).unwrap();
+        thread::spawn(move || receive_from_socket(client_sock, msg_tx));
+
+        let s: String = msg_rx.recv().unwrap();
+        assert_eq!(s, "Hello client");
+    }
+
+    #[test]
+    fn send_basic() {
+        let (event_tx, event_rx) = mpsc::channel();
+
+        let (tx, rx) = mpsc::channel();
+        let addr = "inproc://send_basic";
+        thread::spawn(move || {
+            let srv_sock = socket_pair1_new().unwrap();
+            srv_sock.listen(addr).unwrap();
+            let msg = srv_sock.recv().unwrap();
+            let s: String = serde_cbor::from_slice(msg.as_slice()).unwrap();
+            tx.send(s).unwrap();
+            srv_sock.close();
+        });
+
+        let client_sock = socket_pair1_new().unwrap();
+        dial(&client_sock, addr).unwrap();
+        thread::spawn(move || send_to_socket(client_sock, event_rx));
+        event_tx.send("Hello server".to_owned()).unwrap();
+
+        let s = rx.recv_timeout(Duration::from_secs(5)).unwrap();
+        assert_eq!(s, "Hello server");
+    }
 }
